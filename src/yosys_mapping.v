979d8b7fcfb2def90c75339896cf7b600996b2c2
devin-macy <dm426317@ohio.edu>
2024-05-30 12:10:50 -0400

    unmapped cell during synthesis workaround

--- src/yosys_mapping.v
+++ src/yosys_mapping.v
@@ -1,24 +1,4 @@
-module \$_ALDFF_PN_ (D, C, L, AD, Q);
-input D, C, L, AD;
-output reg Q;
-
-wire RN, SN;
-wire L_N;
-
-\$_OR_ R_NAND ( .Y(RN), .A(L), .B(AD) );
-\$_NOT_ NAND_NOT ( .A(L), .Y(L_N));
-\$_NAND_ S_NAND ( .Y(SN), .A(L_N), .B(AD) );
-
-\$_DFFSR_PNN_ SRFF (.C(C), 
-    .S(SN), 
-    .R(RN), 
-    .D(D), 
-    .Q(Q)
-);
-
-endmodule
-
-module \$_ALDFF_PP_ (D, C, L, AD, Q);
+module \$_ALDFF_PPP_ (D, C, L, AD, Q);
 input D, C, L, AD;
 output reg Q;
 
